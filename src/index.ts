c9e7d83132d1abe235c2562119ee2bf557986676
Kim Sun Woo <sophianet.sun@gmail.com>
2025-05-09 08:51:55 +0900

    🚀 For package version

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,4 @@ import {
   closeSession,
 } from "./session";
 
-export const main = async () => {
-  const client = new IoTDBClient("localhost", 6667);
-  const sessionId = await getSession(client, "root", "root", "UTC");
-  const result = await getQuery(
-    client,
-    sessionId,
-    "select * from root.enerdot.ems.*"
-  );
-  console.log("Query result:", result);
-  await closeSession(client, sessionId);
-  console.log("Session closed successfully", sessionId.toString());
-  await client.closeConnection();
-  console.log("Connection closed");
-};
-
-main();
+export { IoTDBClient, getSession, parseTS, parseValue, getQuery, closeSession };
